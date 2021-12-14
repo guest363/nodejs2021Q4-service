8c07c7cba943ba8b2e03b5916439108559801d30
Oleg Moiseev <guest363@gmail.com>
2021-12-15 00:47:00 +0300

    Fix: tsdoc descripe params

--- src/resources/boards/api/get.ts
+++ src/resources/boards/api/get.ts
@@ -3,7 +3,9 @@ import { boardService } from '../board.service';
 
 /**
  * Endpoint для получения всех досок
- *
+ * 
+ * @param _request - FastifyRequest is an instance of the standard http or http2 request objects. It defaults to http.IncomingMessage, and it also extends the relative request object.
+ * @param reply - FastifyReply is an instance of the standard http or http2 reply types. It defaults to http.ServerResponse, and it also extends the relative reply object.
  * @returns массив всех досок
  */
 export const get = async (_request: FastifyRequest, reply: FastifyReply) => {
